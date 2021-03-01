b52d1f676c8bcdc55e826b0beff917147c1e8e0e
ceylinbrooks <ceylinbrooks@gmail.com>
2021-03-01 18:43:11 -0500

    working on server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const PORT = process.env.PORT;
 
 app.use(express.static('.public'));
 app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
 
-app.get('');
+app.get('/hello', (req, res) => {
+  const helloObject = {};
+  res.render('/pages/show.ejs', helloObject);
+});
 
+app.post('/create', (req, res) => {
+  
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT,() => console.log(`The ${PORT} is live`))
